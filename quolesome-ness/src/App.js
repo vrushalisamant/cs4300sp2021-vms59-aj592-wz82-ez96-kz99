c8bf14d5edee770f2152c2f2dbb92ae8c3769628
Anya Ji <aj592@cornell.edu>
2021-04-17 21:54:13 -0400

    app.js

--- quolesome-ness/src/App.js
+++ quolesome-ness/src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Output from "./Output";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Image from "react-bootstrap/Image";
 
 function App() {
+  const [isOutput, setOutput] = useState(true); //TODO: set this on clicking search
+  
+  //TODO: search info
+  const searchInfo = {
+    text: "oof",
+    categories: ["life", "sad"],
+    emoji: '😢'
+  };
+
+  const content = isOutput ? <Output searchInfo={searchInfo} /> : <></>; //TODO: input page
   return (
     <div className="App">
-     <Output />
+      <Image src="logo192.png" roundedCircle />
+      <h1>QUOLESOME-NESS</h1>
+      {content}
     </div>
   );
 }
