36a1bbde560c7aa3d20836d6564525b6f6962b83
Kai Zou <zkjzou@outlook.com>
2021-04-20 15:02:12 -0400

    take out 404.template

--- app/__init__.py
+++ app/__init__.py
@@ -26,10 +26,6 @@ app.register_blueprint(irsystem)
 # Initialize app w/SocketIO
 socketio.init_app(app)
 
-# HTTP error handling
-@app.errorhandler(404)
-def not_found(error):
-  return render_template("404.html"), 404
 
 @app.route('/', methods=['GET'])
 def index():
